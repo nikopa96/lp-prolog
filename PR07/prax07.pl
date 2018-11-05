b8d6ae1d3382449f8b6972e9badeb86ec6e9b0fb
nikopa <nikopa@ttu.ee>
2018-11-05 10:43:48 +0200

    PR07

--- PR07/prax07.pl
+++ PR07/prax07.pl
@@ -1,21 +1,38 @@
-on_eriliik(loom, elusloend).
-on_eriliik(taim, elusloend).
+% on_eriliik(loom, elusloend).
+% on_eriliik(taim, elusloend).
 
-on_eriliik(selgroogne, loom).
-on_eriliik(lehm, selgroogne).
+% on_eriliik(selgroogne, loom).
+% on_eriliik(lehm, selgroogne).
 
-on_eriliik(selgrootu, loom).
-on_eriliik(puuk, selgrootu).
-on_eriliik(kapsauss, selgrootu).
+% on_eriliik(selgrootu, loom).
+% on_eriliik(puuk, selgrootu).
+% on_eriliik(kapsauss, selgrootu).
 
-on_eriliik(oistaim, taim).
+% on_eriliik(oistaim, taim).
 
-on_eriliik(mitteoistaim, taim).
-on_eriliik(kapsas, mitteoistaim).
+% on_eriliik(mitteoistaim, taim).
+% on_eriliik(kapsas, mitteoistaim).
 
-eats(oistaim, mitteoistaim).
-eats(selgrootu, oistaim).
-eats(selgroogne, selgrootu).
+on_eriliik(s,d).
+on_eriliik(t,d).
+on_eriliik(s1,s).
+on_eriliik(s2,s).
+on_eriliik(s11,s1).
+on_eriliik(s12,s1).
+on_eriliik(s21,s2).
+on_eriliik(s22,s2).
+on_eriliik(t1,t).
+on_eriliik(t2,t).
+on_eriliik(t21,t2).
+on_eriliik(t22,t2).
+
+% eats(oistaim, mitteoistaim).
+% eats(selgrootu, oistaim).
+% eats(selgroogne, selgrootu).
+
+eats(t1,t2).
+eats(s2,t1).
+eats(s1,s2).
 
 is_terminal(Parent, Node):-
     on_eriliik(Node, Parent),
@@ -35,13 +52,13 @@ count_terminals_alam(Node, [Node]):-
 count_terminals(Node, Terminals, Count):-
     count_terminals_alam(Node, Terminals), length(Terminals, Count).
 
-find_extinction(Node, NextNode):-
-    eats(Node, NextNode).
-find_extinction(Node, NextNode):-
-    eats(Node, MiddleNode), 
-    find_extinction(MiddleNode, NextNode).
+find_extinction(Node, PreviousNode):-
+    eats(PreviousNode, Node).
+find_extinction(Node, PreviousNode):-
+    eats(MiddleNode, Node),
+    find_extinction(MiddleNode, PreviousNode).
 
-extinction_alam_1(Node, NewList):-
+extinction_alam_1(Node, [Node|NewList]):-
     findall(Extinction, find_extinction(Node, Extinction), NewList).
 
 extinction_alam_2([], []).
